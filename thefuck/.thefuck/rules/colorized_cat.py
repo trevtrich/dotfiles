050958ad83ca7eaf2ecc0e7b13e253d98cdc76ba
Trevor Richardson <richardson.trev@gmail.com>
2017-09-19 21:04:17 -0500

    Original removal of dotfile directories that I dont use

--- thefuck/.thefuck/rules/colorized_cat.py
+++
@@ -1,9 +0,0 @@
-def match(command):
-    return (command.script.startswith('ccat ')
-                       and ('source-highlight: could not find a language definition for input file' in command.stderr))
-
-def get_new_command(command, settings):
-    return 'cat '.format(command.script)
-
-enabled_by_default = True
-requires_output = True
