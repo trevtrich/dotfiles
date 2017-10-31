b9bb19b3f26c8c9769aa199bb4bcca102bdf8da9
Trevor Richardson <trevor@gaincompliance.com>
2017-10-31 08:32:18 -0500

    No longer using gpg init script for handling sessions.
    For versions of gpg > 2.1 this is now handled automatically: https://www.gnupg.org/faq/whats-new-in-2.1.html#autostart

--- osx/source.sh
+++ osx/source.sh
@@ -6,6 +6,6 @@
 #shellcheck disable=SC1090
 source "$(brew --prefix nvm)/nvm.sh"
 #shellcheck disable=SC1090
-source ~/.files/gpg/init.sh
+#source ~/.files/gpg/init.sh
 #shellcheck disable=SC1090
 [[ -s ~/.avn/bin/avn.sh ]] && source ~/.avn/bin/avn.sh
