fe9af630e3fdf6d3a28845108a6ed74dac5b1860
Trevor Richardson <richardson.trev@gmail.com>
2018-04-14 11:56:36 -0500

    feat(completion): added npm completion

--- bash/completion.sh
+++ bash/completion.sh
@@ -11,15 +11,21 @@ if which docker > /dev/null 2>&1; then
     . ~/.files/docker/bash_completion.sh
 fi
 
-#completion of grunt commands
+# completion of grunt commands
 if which grunt > /dev/null 2>&1; then
     eval "$(grunt --completion=bash)"
 fi
 
-#completion of rake commands
+# completion of rake commands
 if which rake > /dev/null 2>&1; then
     #shellcheck disable=SC1090
     . ~/.files/ruby/rake-completion.sh
+fi
+
+# completion of npm commands
+if which npm > /dev/null 2>&1; then
+    #shellcheck disable=SC1090
+    source <(npm completion)
 fi
 
 #shellcheck disable=SC1090
