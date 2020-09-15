4356c5243a0631e6c32f0ad37e06011d1c82a338
Trevor Richardson <trevor@gaincompliance.com>
2020-09-15 09:50:20 -0500

    fix(bash-completion): blessed technique has pipe fail
    - still not sure what the issue is here, but writing to a file works just fine so rolling with that for now. couldn't find any good links to add in here even for others experiencing the same issue.

--- bash/completion.sh
+++ bash/completion.sh
@@ -25,7 +25,8 @@ fi
 # completion of npm commands
 if which npm > /dev/null 2>&1; then
     #shellcheck disable=SC1090
-    source <(npm completion)
+    #source <(npm completion)
+    npm completion > /usr/local/etc/bash_completion.d/npm
 fi
 
 # global fuzzy completion
