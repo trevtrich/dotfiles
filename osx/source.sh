f07d1c9470dc78302875aa32ed01193c89396a98
Trevor Richardson <trevor@gaincompliance.com>
2022-09-08 23:27:41 -0500

    fix(nvm): homebrew path

--- osx/source.sh
+++ osx/source.sh
@@ -6,9 +6,9 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" == darwin* ]] || return 1
 
-export NVM_DIR="$(realpath $HOME/.nvm)"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 [[ -s ~/.avn/bin/avn.sh ]] && source ~/.avn/bin/avn.sh
 
