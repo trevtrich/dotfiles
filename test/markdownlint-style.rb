fffd4fdb7963ef731f11eb21b18b410061b19d1a
Trevor Richardson <trevor@gaincompliance.com>
2023-10-05 13:43:38 -0500

    test: remove all as was not using

--- test/markdownlint-style.rb
+++
@@ -1,3 +0,0 @@
-all
-rule 'no-trailing-punctuation', :punctuation => '.,;:!'
-exclude_rule 'commands-show-output'
--- test/metadata.rb
+++
@@ -1,6 +0,0 @@
-name 'dotfiles'
-maintainer 'Matt Travi'
-maintainer_email 'programmer@travi.org'
-description 'Installs the dotfiles to prepare for running automated verification'
-
-depends 'git', '~> 4.3'
