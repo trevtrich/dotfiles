0350699fa85ce10435c0edd56d2b3afb8ed77fd6
Trevor Richardson <trevor@gaincompliance.com>
2023-10-05 13:45:15 -0500

    fix(windows): removed as unused

--- windows/setup.cmd
+++
@@ -1,1 +0,0 @@
-@powershell -NoProfile -ExecutionPolicy Unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin && cinst '%~dp0%packages.config' -y
--- windows/source.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Windows-only stuff. Abort if not Windows.
-[[ -n "$WINDIR" ]] || return 1
-
-#shellcheck disable=SC1090
-. ~/.files/windows/ssh-agent.sh
