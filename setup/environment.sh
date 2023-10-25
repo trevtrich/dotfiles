2655c857044610c1d351739656982d2ee7c9c4bf
Trevor Richardson <richardson.trev@gmail.com>
2023-10-25 13:08:58 +0000

    initial work for linux machine with homebrew and zsh

--- setup/environment.sh
+++ setup/environment.sh
@@ -11,4 +11,5 @@ if [ ! -d ~/bin ]; then
 fi
 
 . ../osx/environment.sh
+. ../linux/environment.sh
 . ../windows/environment.sh
--- setup/software.sh
+++ setup/software.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-/bin/bash ../osx/software.sh || return 1
-/bin/bash ../windows/software.sh || return 1
-
-if -d ~/.rbenv > /dev/null 2>&1; then
-    rbenv install "$(cat ~/.rbenv/version)" --skip-existing
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    /bin/bash ../linux/software.sh || return 1
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    /bin/bash ../osx/software.sh || return 1
+elif [[ "$OSTYPE" == "win32" ]]; then
+    echo Windows isnt supported yet
+else
+    echo Yuh oh, we dont know what OS you on.
 fi
 
-if which gem > /dev/null 2>&1; then
-    heading 'Ruby Gems'
-    gem update
-    gem cleanup
-fi
-
-if which npm > /dev/null 2>&1; then
-    heading "NPM Update"
-    npm update -g
-fi
-
-if which vim > /dev/null 2>&1; then
-    heading "Vundle Install"
-    vim +PluginInstall! +qall
-fi
+#if -d ~/.rbenv > /dev/null 2>&1; then
+#    rbenv install "$(cat ~/.rbenv/version)" --skip-existing
+#fi
+#
+#if which gem > /dev/null 2>&1; then
+#    heading 'Ruby Gems'
+#    gem update
+#    gem cleanup
+#fi
+#
+#if which npm > /dev/null 2>&1; then
+#    heading "NPM Update"
+#    npm update -g
+#fi
+#
+#if which vim > /dev/null 2>&1; then
+#    heading "Vundle Install"
+#    vim +PluginInstall! +qall
+#fi
