fe3f87dbb3aa0475af66c2b45181dc9faffe706c
Trevor Richardson <richardson.trev@gmail.com>
2023-10-27 03:31:22 +0000

    feat(linux): finishing up initial setup for zsh and linux

---
+++ linux/environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" == linux-* ]] || return 1
+
+heading 'Configuring Linux'
+
+# Ask for the administrator password upfront
+sudo -v
+
+###############################################################################
+# Bash                                                                        #
+###############################################################################
+
+
+###############################################################################
+# General UI/UX                                                               #
+###############################################################################
+
+echo "Done. Note that some of these changes require a logout/restart to take effect."
---
+++ linux/software.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+#shellcheck disable=SC1090
+. ~/.files/setup/functions.sh
+
+set -euo pipefail
+IFS=$'\n\t'
+
+heading "Setting up linux software"
+
+if [[ ! "$(type -P zsh)" ]]; then
+    echo you gotta apt-get zsh first
+fi
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Install Homebrew.
+if [[ ! "$(type -P brew)" ]]; then
+    heading "Installing Homebrew"
+
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+    test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+fi
+
+heading 'Brew bundle'
+brew update
+brew cleanup
+brew doctor || exit 1
+
+brew bundle --file=~/.files/linux/Brewfile
+
+brew upgrade
+brew cleanup
