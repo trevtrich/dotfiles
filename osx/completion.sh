498722f094a2f0f53be1a394fb6c70ba23907143
Trevor Richardson <trevorr@MacBook-Pro.localdomain>
2024-11-07 11:03:13 -0600

    Updates after moving to another new machine

--- osx/completion.sh
+++ osx/completion.sh
@@ -2,8 +2,3 @@
 
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" == darwin* ]] || return 1
-
-if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
-    #shellcheck disable=SC1090
-   . "$(brew --prefix)/etc/bash_completion"
-fi
