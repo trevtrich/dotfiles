0c7d3752413def14ab14f8aa5f8808f165eb6bde
Trevor Richardson <trevor@getclaimable.com>
2025-02-21 09:34:45 -0600

    fix(input): vim mode change cursor when normal versus insert

--- zsh/input.sh
+++ zsh/input.sh
@@ -1,1 +1,16 @@
-bindkey -M viins jk vi-cmd-mode
+# Change cursor shape for different vi modes.
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] || [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'  # Block cursor in normal mode
+  elif [[ ${KEYMAP} == main ]] || [[ ${KEYMAP} == viins ]] || [[ ${KEYMAP} = '' ]] || [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'  # Beam cursor in insert mode
+  fi
+}
+zle -N zle-keymap-select
+
+zle-line-init() {
+  zle -K viins  # Start in insert mode
+}
+zle -N zle-line-init
+
+bindkey -M viins 'jk' vi-cmd-mode
